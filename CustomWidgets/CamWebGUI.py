ac1a4bc7e1d1650f02ebf9d1ae62587e783efbec
Maximilian <ge59nif@mytum.de>
2021-12-31 11:38:29 +0100

    change IP

--- CustomWidgets/CamWebGUI.py
+++ CustomWidgets/CamWebGUI.py
@@ -161,9 +161,5 @@ class CamWebGUI(App):
 
 if __name__ == "__main__":
     # starts the webserver
-    # optional parameters
-    # start(MyApp,address='127.0.0.1', port=8081, multiple_instance=False,enable_file_cache=True, update_interval=0.1, start_browser=True)
-    start(CamWebGUI, debug=False, address='130.183.94.217', port=8081, start_browser=False, multiple_instance=False)        
-    #start(MyApp, debug=False, address='192.1.2.20', port=8081, start_browser=False, multiple_instance=False) 
-    #start(MyApp, debug=False, address='0.0.0.0', port=8081, start_browser=False, multiple_instance=False)        
+    start(CamWebGUI, debug=False, address='127.0.0.1', port=8081, start_browser=False, multiple_instance=False)        
         
